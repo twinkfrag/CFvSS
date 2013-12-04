f9d9e49f638499d1a83525e0da47566e5771e29d
あると <mail@twinkfrag.net>
2013-12-04 15:29:18 +0900

    bmpオブジェクトの破棄

--- CocoroFunctionVerScreenShot/ScreenShot.cs
+++ CocoroFunctionVerScreenShot/ScreenShot.cs
@@ -9,31 +9,29 @@ namespace CocoroFunctionVerScreenShot
         /// <summary>
         /// 撮影
         /// </summary>
-        public static Bitmap Copy()
+        public static void Shot()
         {
+            //アクティブウィンドウの取得
             IO.RECT r;
             IntPtr active = Program.InObject.active;
             IO.GetWindowRect(active, out r);
             Rectangle rect = new Rectangle(r.left, r.top, r.right - r.left, r.bottom - r.top);
 
-            Bitmap bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
-            using (Graphics g = Graphics.FromImage(bmp))
+            using (Bitmap bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb))
             {
-                g.CopyFromScreen(rect.X, rect.Y, 0, 0, rect.Size, CopyPixelOperation.SourceCopy);
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    g.CopyFromScreen(rect.X, rect.Y, 0, 0, rect.Size, CopyPixelOperation.SourceCopy);
+                }
+                Save(bmp, Program.InObject.fullPath);
             }
-
-            return bmp;
-        }
-        public static void Shot()
-        {
-            Save(Copy(), Program.InObject.fullPath);
-
         }
 
         public static void Save(Bitmap bmp, string filename)
         {
-            System.Text.RegularExpressions.Regex r = new System.Text.RegularExpressions.Regex(@".*\\");
-            string dir = r.Match(filename).ToString();
+            //filenameからディレクトリを抜き出し存在チェック
+            System.Text.RegularExpressions.Regex dirEx = new System.Text.RegularExpressions.Regex(@".*\\");
+            string dir = dirEx.Match(filename).ToString();
             if (!System.IO.Directory.Exists(dir)) System.IO.Directory.CreateDirectory(dir);
 
             using (System.IO.FileStream file = new System.IO.FileStream(filename, System.IO.FileMode.Create))
